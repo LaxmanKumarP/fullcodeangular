dff1b0a30f38e6e491d8799f93b15e1ddef6bdbc
LaxmanKumarP <laxmanncde@gmail.com>
2024-07-14 23:44:16 -0400

    changes of mockjson integration

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { ITodo } from '../../shared/interfaces/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+  todos!: ITodo[];
+  constructor(private todoService:TodoService){}
+  ngOnInit(){
+    this.todoService.getTodos().subscribe(data => {
+      this.todos = data;
+      console.log(this.todos);
+    });
+  }
 
 }
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AddEditTodoComponent } from './add-edit-todo/add-edit-todo.component';
+import { TodoService } from './todo.service';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,8 +13,10 @@ import { AddEditTodoComponent } from './add-edit-todo/add-edit-todo.component';
     AddEditTodoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
+  providers:[TodoService],
   exports:[ TodoListComponent,
     AddEditTodoComponent]
 })
